1ab8ed19846c9ef139097bbd4896db5481240c87
Thuc Tran <thuc.ttk@gmail.com>
2023-03-10 23:08:07 +0700

    [add] project

---
+++ Assets/Scripts/JSONDemo.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.IO;
+
+public class JSONDemo : MonoBehaviour
+{
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI playerLevel;
+    public TextMeshProUGUI playerGold;
+    private string json;
+
+    void Start()
+    {
+        PlayerData playerData = new PlayerData
+        {
+            Name = "Cat",
+            Level = 1,
+            Gold = 100
+        };
+        json = JsonUtility.ToJson(playerData);
+        Debug.Log(json);
+    }
+
+    public void LoadPlayerData()
+    {
+        string loadJSON = File.ReadAllText(Application.dataPath + "/Data/saveFile.json");
+        if(loadJSON != null)
+        {
+            PlayerData loadedPlayerData = JsonUtility.FromJson<PlayerData>(loadJSON);
+            playerName.text = "Player Name: " + loadedPlayerData.Name;
+            playerLevel.text = "Player Level: " + loadedPlayerData.Level;
+            playerGold.text = "Player Gold: " + loadedPlayerData.Gold;
+        } 
+    }
+
+    public void SaveJsonFile()
+    {
+        File.WriteAllText(Application.dataPath + "/Data/saveFile.json", json);
+    }
+
+    public void UpdateData()
+    {
+        PlayerData updateData = new PlayerData
+        {
+            Name = "Dog",
+            Level = 2,
+            Gold = 1000
+        };
+        playerName.text = "Player Name: " + updateData.Name;
+        playerLevel.text = "Player Level: " + updateData.Level;
+        playerGold.text = "Player Gold: " + updateData.Gold;
+        json = JsonUtility.ToJson(updateData);
+        SaveJsonFile();
+    }
+
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class PlayerData
+{
+    public string Name;
+    public int Level;
+    public int Gold;
+}
